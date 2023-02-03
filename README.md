4b7b79449787daa179f557da781f2ee1439306d4
Austin Doss <adoss@matterport.com>
2023-02-03 14:19:35 -0600

    added Bundle SDK files and initial scene

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Bundle-SDK-Vite
  
+1. Install with `yarn` or `npm install`.
+1. Run with `yarn dev` or `npm run dev`.
--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { MpSdk } from "../public/mp_bundle/sdk";
+
+declare global {
+  interface Window {
+    MP_SDK: any;
+  }
+}
+
+async function handleShowcaseLoad() {
+  const showcaseIframe = document.getElementById("showcase_iframe") as HTMLIFrameElement;
+  try {
+    if (showcaseIframe.contentWindow) {
+      let sdk = await showcaseIframe.contentWindow.MP_SDK.connect(
+        showcaseIframe
+      );
+      sdk.App.state.subscribe((state: any) => {
+        if (state.phase === sdk.App.Phase.PLAYING) {
+          onSdkConnect(sdk);
+        }
+      });
+    }
+  } catch (e) {
+    console.error(e);
+    return;
+  }
+}
+
+async function onSdkConnect(sdk: MpSdk) {
+  console.log('SDK Connected: ', sdk)
+  // run your SDK code here
+
+  var [sceneObject] = await sdk.Scene.createObjects(1);
+  var logoNode = sceneObject.addNode("logoNode");
+  var lightNode = sceneObject.addNode("lightNode");
+
+  var logoInitialInputs = {
+    url: "/assets/Matterport_Logo.gltf",
+    visible: true,
+    localScale: {
+      x: 0.1,
+      y: 0.1,
+      z: 0.1,
+    },
+  };
+
+  var directionalLightInitialInputs = {
+    enabled: true,
+    color: {
+      r: 1,
+      g: 1,
+      b: 1,
+    },
+    intensity: 2,
+    target: {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+    debug: false,
+  };
+
+  var ambientLightInitialInputs = {
+    intensity: 1,
+    color: {
+      r: 1,
+      g: 1,
+      b: 1,
+    },
+  }
+
+  logoNode.addComponent(
+    "mp.gltfLoader",
+    logoInitialInputs
+  );
+
+  var lightComponent = lightNode.addComponent(
+    "mp.directionalLight",
+    directionalLightInitialInputs
+  );
+  
+  lightNode.addComponent(
+    "mp.ambientLight",
+    ambientLightInitialInputs
+  );
+
+  sceneObject.start();
+  logoNode.obj3D.position.set(0, 0, 0);
+  lightComponent.light.target = logoNode.obj3D;
+
+  const tick = function () {
+    requestAnimationFrame(tick);
+    logoNode.obj3D.rotation.y += 0.01;
+  };
+  tick();
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <iframe id={'showcase_iframe'} width={'800px'} height={'600px'} frameBorder={0} src='./mp_bundle/showcase.html?m=j4RZx7ZGM6T&applicationKey=5n1rihzdbgxus7k1exfne27ac&play=1' onLoad={handleShowcaseLoad}></iframe>
     </div>
   )
 }
